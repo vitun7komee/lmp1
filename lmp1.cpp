9a208999b796b123adde14956606616a037d9524
vitun7komee <118392333+vitun7komee@users.noreply.github.com>
2023-05-10 21:00:04 +0300

    Add files via upload

---
+++ lmp1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<int, int> fib_map;
+
+int fib(int n) {
+    if (n < 2) return n; // базовый случай
+    if (fib_map.count(n) == 1) return fib_map[n]; // если результат уже вычислен, возвращаем его
+    int result = fib(n - 1) + fib(n - 2); // иначе вычисляем результат
+    fib_map.insert({ n, result }); // сохраняем результат
+    return result;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int result = fib(n);
+
+    cout << "Fibonacci number at position " << n << " is: " << result << endl;
+
+    return 0;
+}
